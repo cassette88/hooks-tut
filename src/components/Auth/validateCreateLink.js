5b853208733e171f6b7234b9cc98cdeefe2f9c9c
Dave <chopin@iJones.local>
2019-08-28 12:54:42 -0400

    initial commit

---
+++ src/components/Auth/validateCreateLink.js
@@ -0,0 +1,20 @@
+export default function validateCreateLink(values) {
+    let errors = {}
+
+    // Description errors
+    if(!values.description) {
+        errors.description = "Description required"
+    } else if(values.description.length < 10) {
+        errors.description = "Please provide a description longer than 10 characters"
+    }
+
+    //URL errors
+    if (!values.url){
+        errors.url = "URL required"
+    } else if (!/^(ftp|http|https):\/\/[^ "]+$/.test(values.url)) {
+        errors.url =  "URL must be valid"
+    }
+
+    return errors;
+
+}
